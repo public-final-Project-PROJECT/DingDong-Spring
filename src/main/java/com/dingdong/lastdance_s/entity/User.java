0e8c7ac9a80c4a01a8d1642a0af66e91f1c12038
RJANG <msgpigeon@gmail.com>
2024-12-23 15:05:11 +0900

    해치웠나?

---
+++ src/main/java/com/dingdong/lastdance_s/entity/User.java
@@ -0,0 +1,28 @@
+package com.dingdong.lastdance_s.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "teacher")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "teacher_id")
+    private Integer id;
+
+    @Column(name = "teacher_name")
+    private String name;
+
+    @Column(name = "teacher_img")
+    private String picture;
+
+    @Column(name = "teacher_email")
+    private String email;
+
+}
+
