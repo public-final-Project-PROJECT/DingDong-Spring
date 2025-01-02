2b392066e76f9fc62c76e33ed07cb851c3189a0a
seo <cgo2711@naver.com>
2025-01-02 18:07:19 +0900

    add

--- src/main/java/com/dingdong/lastdance_s/repository/SeatRepository.java
+++ src/main/java/com/dingdong/lastdance_s/repository/SeatRepository.java
@@ -3,6 +3,8 @@ package com.dingdong.lastdance_s.repository;
 
 import com.dingdong.lastdance_s.entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,8 @@ import java.util.List;
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Integer> {
     List<Seat> findByClassId(int classId);
+
+    @Modifying
+    @Query("UPDATE Seat s SET s.rowId = :rowId, s.columnId = :columnId WHERE s.classId = :classId AND s.studentId = :studentId")
+    void updateSeatPosition(Integer classId, Integer studentId, Integer rowId, Integer columnId);
 }
