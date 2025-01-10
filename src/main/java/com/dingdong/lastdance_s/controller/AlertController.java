fb9f59215ea6c1862a16effab24159a56e1e1cd5
temless <ljw980722@naver.com>
2025-01-10 15:42:23 +0900

    s

---
+++ src/main/java/com/dingdong/lastdance_s/controller/AlertController.java
@@ -0,0 +1,14 @@
+package com.dingdong.lastdance_s.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/alert")
+public class AlertController {
+
+
+
+
+
+}
