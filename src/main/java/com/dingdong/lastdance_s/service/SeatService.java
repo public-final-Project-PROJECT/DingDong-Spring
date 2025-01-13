28fd771f9e053a8cb76d127db48c3a7e4ea65d39
seo <cgo2711@naver.com>
2025-01-13 18:06:53 +0900

    add

--- src/main/java/com/dingdong/lastdance_s/service/SeatService.java
+++ src/main/java/com/dingdong/lastdance_s/service/SeatService.java
@@ -8,8 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class SeatService {
@@ -60,7 +59,61 @@ public class SeatService {
     // 좌석 저장
     public List<Map<String, Object>> saveSeat(List<Map<String, Object>> newSeats) {
         System.out.println("좌석 저장에 넘어온 newSeats :: " + newSeats);
-        // seatRepository.save(newSeats);
-        return newSeats;
+        List<Map<String, Object>> savedSeats = new ArrayList<>();
+
+        for (Map<String, Object> seatData : newSeats) {
+            try {
+                // studentId 변환
+                Object studentIdObj = seatData.get("studentId");
+                Integer studentId = (Integer) studentIdObj;
+
+
+                // 데이터 가져오기
+                int columnId = Integer.parseInt(seatData.get("columnId").toString());
+                int rowId = Integer.parseInt(seatData.get("rowId").toString());
+                int classId = 2; // 예제의 경우 고정값, 필요시 동적으로 처리
+
+                // Seat 객체 생성 및 저장
+                Seat seat = new Seat(classId, columnId, rowId, studentId);
+                System.out.println("여기이이이 4개 찍혀야댐 :: " + seat.toString());
+                Seat lastSeat = seatRepository.save(seat);
+
+                // 저장된 좌석을 Map으로 변환
+                Map<String, Object> seatMap = new HashMap<>();
+                seatMap.put("seatId", lastSeat.getSeatId());
+                seatMap.put("classId", lastSeat.getClassId());
+
+                seatMap.put("columnId", lastSeat.getColumnId());
+                seatMap.put("rowId", lastSeat.getRowId());
+                seatMap.put("studentId", lastSeat.getStudentId());
+
+                savedSeats.add(seatMap);
+            } catch (Exception e) {
+                System.out.println("좌석 저장 중 오류 발생: " + e.getMessage());
+            }
+        }
+
+        return savedSeats;
+    }
+
+    public List<Seat> insertSeats(List<Map<String, Object>> newSeats) {
+        System.out.println("새로 만든 seat 서비스 : " + newSeats);
+
+        List<Seat> insertedSeats = new ArrayList<>();
+
+        for (Map<String, Object> seatData : newSeats) {
+
+            Seat seat = new Seat();
+            seat.setClassId(Integer.parseInt(seatData.get("classId").toString()));
+            seat.setColumnId(Integer.parseInt(seatData.get("columnId").toString()));
+            seat.setRowId(Integer.parseInt(seatData.get("rowId").toString()));
+            seat.setStudentId(Integer.parseInt(seatData.get("studentId").toString()));
+
+            Seat savedSeat = seatRepository.save(seat);
+            insertedSeats.add(savedSeat);
+        }
+
+        return insertedSeats;
     }
 }
+
