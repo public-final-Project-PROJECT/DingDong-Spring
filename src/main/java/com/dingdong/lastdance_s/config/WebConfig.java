a0182bf8c265e09966d2d4140b4a7944b4285574
R-JANG-98 <msgpigeon@gmail.com>
2025-01-13 17:47:42 +0900

    Update WebConfig.java

--- src/main/java/com/dingdong/lastdance_s/config/WebConfig.java
+++ src/main/java/com/dingdong/lastdance_s/config/WebConfig.java
@@ -11,9 +11,8 @@ public class WebConfig implements WebMvcConfigurer
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:1557")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
